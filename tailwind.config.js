06a5113e1ea93e6b58ddbab55636bfd1bdd5b4c8
Gustav Beier <gustavbeier@gmail.com>
2022-10-10 22:47:38 -0300

    Basic HTML structure and unsaturated green color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,20 @@ module.exports = {
             sans: ["'Fira Sans'", ...defaultTheme.fontFamily.sans],
         },
         extend: {
-            boxShadow: {
-                hard: "4px 4px black",
+            colors: {
+                laurel: {
+                    DEFAULT: "#C4C7C2",
+                    50: "#FEFEFE",
+                    100: "#F2F3F2",
+                    200: "#DBDDDA",
+                    300: "#C4C7C2",
+                    400: "#A5A9A2",
+                    500: "#868C82",
+                    600: "#686D64",
+                    700: "#494D47",
+                    800: "#2B2D2A",
+                    900: "#121311",
+                },
             },
         },
     },
